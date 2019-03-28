625ec16a869a6faff037e5e22aeec02c4885cb1b
zhangalong <>
2019-03-28 18:19:34 +0800

    no message

--- mvvm/src/main/java/com/zalyyh/mvvm/viewadapter/ViewAdapter.java
+++ mvvm/src/main/java/com/zalyyh/mvvm/viewadapter/ViewAdapter.java
@@ -1,5 +1,28 @@
 package com.zalyyh.mvvm.viewadapter;
 
+import android.databinding.BindingAdapter;
+import android.view.View;
+
 public class ViewAdapter {
+    /**
+     * 设置RecyclerView是否可以滑动
+     * */
+//    @BindingAdapter(value = {"android:NestedScrollingEnabled"}, requireAll = false)
+//    public static void setNestedScrollingEnabled(RecyclerView view, boolean b) {
+//        view.setNestedScrollingEnabled(b);
+//    }
+
+    /**
+     * 设置RecyclerView是否可以滑动
+     */
+    @BindingAdapter(value = {"android:LeftPadding"}, requireAll = false)
+    public static void setPaddingLeft(View view, int left) {
+        view.setPadding(left, view.getPaddingTop(), view.getPaddingRight(), view.getPaddingBottom());
+    }
+    @BindingAdapter(value = {"android:visibility"}, requireAll = false)
+    public static void setvisibility(View view, int visibility) {
+        view.setVisibility(visibility);
+    }
+
     
 }
